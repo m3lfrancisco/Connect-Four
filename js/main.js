84b0278dcf75aad6415225404400e7e2a03aeb2a
mfrancisco <francisco.melanie@gmail.com>
2022-04-13 18:07:58 -0700

    update colors in const mark

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 /*----- constants -----*/
 const mark = {
-    '1': 'yellow',
-    '-1': 'red',
-    '0': 'white'
+    '1': '#F2F680', // yellow,
+    '-1': '#EF7179', // red
+    '0': '#FAF4E0' // white
 };
 
 const totalMoves = 42;
@@ -17,12 +17,12 @@ let yellowScore = 0;
 let redScore = 0;
 
 /*----- cached element references -----*/
-const playButtons = document.querySelectorAll('#play-buttons > div');
-const gameStatus = document.getElementById('#game-status');
+const playButtons = [...document.querySelectorAll('#play-buttons > div')];
+const gameStatus = document.getElementById('message');
 const yellowPlayer = document.querySelector('.yellow-score');
 const redPlayer = document.querySelector('.red-score');
 // const music = document.getElementById('#sound');
-const playSound = document.getElementById('#sound-on');
+// const playSound = document.getElementById('#sound-on');
 // const resetButton = document.getElementById('#reset-game');
 // const winSound
 
@@ -31,14 +31,14 @@ const playSound = document.getElementById('#sound-on');
 // document.getElementById('reset-game').addEventListener('click', init);
 // document.getElementById('sound-on').addEventListener('click', toggleSound);
 
-// $('#play-buttons').on('click', 'div', handleMove);
+$('#play-buttons').on('click', 'div', handleMove);
 $('#reset-game').on('click', init);
-$('#sound-on').on('click', toggleSound);
+// $('#sound-on').on('click', toggleSound);
 
 /*----- functions -----*/
-function toggleSound() {
-     playSound.on ? music.play() : music.pause();
-}
+// function toggleSound() {
+//      playSound.on ? music.play() : music.pause();
+// }
 
 init();
 
@@ -56,7 +56,7 @@ function init() {
     movesMade = 0;
     winner = null;
     render();
-}
+};
 
 function render() {
     // iterate over each column in the board
@@ -73,18 +73,18 @@ function render() {
 
     // render game status message
     if (winner === 'T') {
-      gameStatus.innerText = "It's a Tie!";  
+      message.innerText = "It's a Tie!";  
     } else if (winner) {
         if (winner === 1) {
-            ++yellowScore
+            yellowScore++
             yellowPlayer.innerText = `${yellowScore}`;
         } else if (winner === -1) {
-            ++redScore
+            redScore++
             redPlayer.innerText = `${redScore}`;
         }
-        gameStatus.innerText = `${winner === 1 ? 'YELLOW' : 'RED'} wins the game!`;
+        message.innerText = `${winner === 1 ? 'YELLOW' : 'RED'} wins the game!`;
     } else {
-        gameStatus.innerText = `It's ${playerTurn === 1 ? 'YELLOW' : 'RED'}'s turn!`;
+        message.innerText = `It's ${playerTurn === 1 ? 'YELLOW' : 'RED'}'s turn!`;
     }
 };
 
@@ -102,6 +102,71 @@ function handleMove(evt) {
     movesMade++;
     playerTurn *= -1;
 
-    winner = getWinner(columnIdx, rowIdx);
+    winner = checkForWin(columnIdx, rowIdx);
     render();
+}
+
+function checkForWin() {
+    // Check for a tie
+    if (movesMade === 42) 
+        return winner = 'T';
+
+    // Check each column for a winner
+    for (let columnIdx = 0; columnIdx <= 6; columnIdx++) {
+        winner = checkColumn(columnIdx);
+        if (winner) break; 
+    }
+    return winner;
+}
+
+// Check index of column in the board 2d array
+function checkColumn(columnIdx) {
+    const columnArr = board[columnIdx];
+    // Check each row in the column for a winner and set winning color's number value to winner variable
+    for (let rowIdx = 0; rowIdx < columnArr.length; rowIdx++) {
+        let winner = checkVertical(columnArr, rowIdx) || checkHorizontal(columnIdx, rowIdx) ||
+            checkDiagonal(columnIdx, rowIdx, 1) || checkDiagonal(columnIdx, rowIdx, -1);
+            if (winner) return winner;
+    }
+    return null;
+};
+
+function checkDiagonal(columnIdx, rowIdx, direction) {
+    // Boundary check
+    if (direction > 0 && columnIdx > 3 || direction > 0 && rowIdx > 2) return null;
+    if (direction < 0 && columnIdx > 3 || direction < 0 && rowIdx < 3) return null;
+    
+    if (Math.abs(board[columnIdx][rowIdx] + board[columnIdx + 1][rowIdx + direction] +
+        board[columnIdx + 2][rowIdx + direction * 2] + board[columnIdx + 3][rowIdx + direction * 3]) === 4) {
+            return board[columnIdx][rowIdx];          
+    } else {
+        return null;
+    }
+};
+
+function checkHorizontal(columnIdx, rowIdx) {
+    // Boundary check
+    if (columnIdx > 3) return null;
+
+    // Add the value of all spaces to the right of columnIdx and look for +4 or -4
+    // then return the value of +4 using Math.abs to get the winner
+    if (Math.abs(board[columnIdx][rowIdx] + board[columnIdx + 1][rowIdx] + 
+        board[columnIdx + 2][rowIdx] + board[columnIdx + 3][rowIdx]) === 4) {
+            return board [columnIdx][rowIdx];
+        } else {
+            return null;
+        }
+};
+
+function checkVertical(columnArr, rowIdx) {
+    // Boundary check
+    if (rowIdx > 2) return null;
+
+    // Add the value of all spaces above row index and look for +4 or -4
+    // then return the value of +4 using Math.abs to get the winner
+    if (Math.abs(columnArr[rowIdx] + columnArr[rowIdx + 1] + columnArr[rowIdx + 2] + columnArr[rowIdx + 3]) === 4) {
+        return columnArr[rowIdx];
+    } else {
+        return null;
+    }
 };
